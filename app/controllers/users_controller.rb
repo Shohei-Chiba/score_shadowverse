4e25aa054106a72f0f10274008fc7461c7a8fe94
EC2 Default User <ec2-user@ip-172-31-26-117.us-east-2.compute.internal>
2018-09-10 13:31:52 +0000

    create Winning percentage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,23 @@ class UsersController < ApplicationController
     
     count = 0
     
+    @elf = Result.where(:name => "エルフ", :user_id => current_user.id)
+    
     names.each do |name|
       @e = Result.where(:user_id => current_user.id, :name => name) 
       @results[count]  = {:pre_win => @e.sum_pre_win, :post_win => @e.sum_post_win, :lose => @e.sum_loose, :count => @e.sum_battle_count }
       count += 1
+    end
+  end
+  
+  def moreinfo
+    @name = params[:name]
+    @decks = ["エルフ","ロイヤル", "ウィッチ","ドラゴン","ネクロマンサー","ヴァンパイア","ビショップ","ネメシス" ]
+    @data = Result.where(:name => params[:name], :user_id => current_user.id)
+    @score = Hash.new
+    
+    @decks.each do |deck|
+      @score[deck.to_s] = {:win => 0, :lose => 0}
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
   resources :decks
   get 'users/show'
+  get 'users/moreinfo'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'test#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
